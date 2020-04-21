02f80c33bf16a66b089d858c18b4af9d91ac3f29
Alek Tobias <alektobias@gmail.com>
2020-04-21 18:02:49 -0300

    feat: challenge done

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,23 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
+    const data = new FormData();
+    uploadedFiles.map(file => data.append('file', file.file, file.name));
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const parsedFiles: FileProps[] = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: String(file.size),
+    }));
+    setUploadedFiles([...uploadedFiles, ...parsedFiles]);
   }
 
   return (
